f996a52ac11255166467d5a7f4aeabc82bb01916
uo295432 <UO295432@uniovi.es>
2024-12-11 00:28:08 +0100

    Add files via upload

--- js/fondo.js
+++ js/fondo.js
@@ -1,16 +1,33 @@
 class Fondo{
+    pais;
+    capital;
+    circuito;
     constructor(pais,capital,circuito){
         this.pais=pais;
         this.capital=capital;
         this.circuito=circuito;
     }
-    obtenerImagen(){
-        var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-        var data=$.getJSON(flickrAPI,{
-            tags:"oviedo",
-            tagmode:"any",
-            format:"json"
-        })
-        $("<img />").attr( "src", data).appendTo( "#imagenes" );
+    obtenerImagen() {
+        const flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+        $.getJSON(flickrAPI, 
+            {
+                tags: this.capital+","+this.pais+","+this.circuito, 
+                tagmode: "any",
+                format: "json"
+            }
+        )
+        .done((data) => {
+            if (data.items.length > 0) {
+                const url = data.items[0].media.m.replace("_m", "_b");
+                $("main").css({
+                    "background-image": `url(${url})`,
+                    "background-size": "cover",
+                    "background-position": "center",
+                    "background-repeat": "no-repeat",
+                    'min-height': '100vh', 
+                    'margin': '0'
+                });
+            }
+        });
     }
 }
